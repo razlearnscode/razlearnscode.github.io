aa2b7a1991f4f4c2e9441b90e7e2526fc2539cb2
razlearnscode <173060286+razlearnscode@users.noreply.github.com>
2025-03-02 22:47:09 +0800

    new mailbox project

---
+++ mail/mail/static/mail/inbox.js
@@ -0,0 +1,52 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+  // Use buttons to toggle between views
+  document.querySelector('#inbox').addEventListener('click', () => load_mailbox('inbox'));
+  document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
+  document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
+  document.querySelector('#compose').addEventListener('click', compose_email);
+
+  // By default, load the inbox
+  load_mailbox('inbox');
+});
+
+function compose_email() {
+
+  // Show compose view and hide other views
+  document.querySelector('#emails-view').style.display = 'none';
+  document.querySelector('#compose-view').style.display = 'block';
+
+  // Clear out composition fields
+  document.querySelector('#compose-recipients').value = '';
+  document.querySelector('#compose-subject').value = '';
+  document.querySelector('#compose-body').value = '';
+
+  // Submit and POST API to send email via the submit button
+  document.querySelector()
+
+
+}
+
+function load_mailbox(mailbox) {
+  
+  // Show the mailbox and hide other views
+  document.querySelector('#emails-view').style.display = 'block';
+  document.querySelector('#compose-view').style.display = 'none';
+
+  // Show the mailbox name
+  document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+
+  // Get mails from Inbox using the API GET call
+  fetch('/emails/inbox')
+  .then(response => response.json())
+  .then(emails => {
+    // Print all emails
+    console.log(emails);
+  });
+
+
+  // Get mails from Sent Mailbox
+
+
+  // Get mails from Archive Mailbox
+}
