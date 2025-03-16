7eea95e8099a5e36775a7d2ed03c8980ffe63730
razlearnscode <173060286+razlearnscode@users.noreply.github.com>
2025-03-16 13:27:23 +0800

    Stronger: Create basic login, logout and register interface

--- CS50w Projects/10. project4/network/views.py
+++ CS50w Projects/10. project4/network/views.py
@@ -10,7 +10,7 @@ from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 import json
 
-from .models import User, Post, Like, Follow
+, Post, Like, Follow
 
 @csrf_exempt
 def index(request):
